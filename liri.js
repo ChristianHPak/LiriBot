632815f37e0c7d8a73b8411202d90454c869f4e6
Christian Pak <iamchristianpak@gmail.com>
2019-06-24 16:27:42 -0600

    movie and concert-this works, need to test other functions and then polish it up

--- liri.js
+++ liri.js
@@ -2,31 +2,46 @@ require("dotenv").config();
 
 // variables
 var keys = require("./keys.js");
-var spotify = new Spotify(keys.spotify);
 var Spotify = require("node-spotify-api");
 var fs = require("fs");
 var axios = require("axios")
+var file_name = "./log.txt";
 
 // varriables user commands
 var user_command = process.argv[2];
-var second_command = process.argv.splice(2).join(" ");
+var second_command = process.argv.splice(3).join(" ");
 
-// spotify this song command
-function spotify_this(song_name) {
-    if (second_command === undefined) {
-        second_command === "Tommorow (feat. Jason Lancaster)"
-    }
-    spotify.search({
-        type: 'track',
-        query: second_command
-    }, function (err, data) {
-        if (err) return console.log('Error occurred: ' + err);
 
-        console.log(data);
+function concert_this() { //works
+    // Run a request with axios to the queryUrl
+    var queryUrl = "https://rest.bandsintown.com/artists/" + second_command + "/events?app_id=codingbootcamp"
+    console.log(queryUrl)
+    // Then create a response with axios to the queryUrl
+    axios.get(queryUrl).then(function (err, response) {
+        if (err) return console.log("An Error Occurred: ", err)
+
+        console.log(response)
     })
 }
 
-function movie_this() {
+// spotify this song command
+function spotify_this() { //doesnt work
+    var spotify = new Spotify(keys.spotify);
+    console.log(spotify)
+    spotify
+        .search({
+            type: 'track',
+            query: 'All the Small Things'
+        })
+        .then(function (response) {
+            console.log(response);
+        })
+        .catch(function (err) {
+            console.log(err);
+        });
+}
+
+function movie_this() { //works
     // Run a request with axios to the OMDB API
     var queryUrl = "http://www.omdbapi.com/?t=" + second_command + "&y=&plot=short&apikey=trilogy";
     console.log(queryUrl)
@@ -34,26 +49,39 @@ function movie_this() {
     // Then create a response with axios to the queryUrl
     axios.get(queryUrl).then(function (err, response) {
         if (err) return console.log("An Error Occurred: ", err)
+
+        console.log(response)
     })
-    console.log(response)
 }
 
-function concert_this(){
-    // Run a request with axios to the queryUrl
-    var queryUrl = "https://rest.bandsintown.com/artists/" + second_command + "/events?app_id=codingbootcamp"
+function do_what() {
 
-    // Then create a response with axios to the queryUrl
-    axios.get(queryUrl).then(function (err, response) {
-        if (err) return console.log("An Error Occurred: ", err)
-    })
-    console.log(response)
-}
-
-function do_what(){
-    
     fs.readFile("./random.txt", "utf8", function (err, response) {
         if (err) return console.log("An Error Occurred: ", err)
 
         console.log(response)
     });
+}
+
+// Choose a user_Command
+switch (user_command) {
+
+    case "concert-this":
+        concert_this();
+        break;
+
+    case "spotify-this-song":
+        spotify_this();
+        break;
+
+    case "movie-this":
+        movie_this();
+        break;
+
+    case "do-what-it-says":
+        do_what();
+        break;
+
+    default:
+        return console.log("Uh-oh something went wrong")
 }

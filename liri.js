6a9f43db726f56014c95bc099f399c2c63f8cb34
Christian Pak <iamchristianpak@gmail.com>
2019-06-25 09:42:05 -0600

    finished up project

--- liri.js
+++ liri.js
@@ -6,30 +6,31 @@ var Spotify = require("node-spotify-api");
 var fs = require("fs");
 var axios = require("axios")
 var moment = require("moment")
-var file_name = "./log.txt";
 
 // varriables user commands
 var user_command = process.argv[2];
+var second_command = process.argv.splice(3).join("+");
+console.log(second_command)
 
 
 
 function concert_this() { //works
-    var second_command = process.argv.splice(3).join("");
+
     // Run a request with axios to the queryUrl
     var queryUrl = "https://rest.bandsintown.com/artists/" + second_command + "/events?app_id=codingbootcamp"
+
     // Then create a response with axios to the queryUrl
     axios.get(queryUrl).then(function (response) {
         console.log("Venue: " + response.data[0].venue.name);
         console.log("City: " + response.data[0].venue.city);
         console.log("Date of the event: " + moment(response.data[0].datetime).format("MM/DD/YYYY"));
     })
-
 }
 
 // spotify this song command
-function spotify_this() { //works
+function spotify_this(second_command) { //works
     var spotify = new Spotify(keys.spotify);
-    var second_command = process.argv.splice(3).join("+");
+
     var queryUrl = 'https://api.spotify.com/v1/search?type=track&limit=1&q=' + second_command;
 
     //     spotify
@@ -40,6 +41,7 @@ function spotify_this() { //works
     //   .catch(function(err) {
     //     console.log(err);
     //   });
+
     spotify
         .request(queryUrl)
         .then(function (response) {
@@ -54,13 +56,6 @@ function spotify_this() { //works
             //album name
             console.log("Album: " + songData.album.name);
             console.log("-----------------------");
-
-            // //adds text to log.txt
-            // fs.appendFile('./log.txt', songData.artists[0].name);
-            // fs.appendFile('./log.txt', songData.name);
-            // fs.appendFile('./log.txt', songData.preview_url);
-            // fs.appendFile('./log.txt', songData.album.name);
-            // fs.appendFile('./log.txt', "-----------------------"); 
         })
         .catch(function (err) {
             console.error('Error occurred: ' + err);
@@ -69,35 +64,41 @@ function spotify_this() { //works
 
 function movie_this() { // works
     // Run a request with axios to the OMDB API
-    var second_command = process.argv.splice(3).join("");
     var queryUrl = "http://www.omdbapi.com/?t=" + second_command + "&y=&plot=short&apikey=trilogy";
     console.log(queryUrl)
 
     // Then create a response with axios to the queryUrl
     axios.get(queryUrl).then(function (response) {
 
-        console.log("Title: " + response.data.Title);
-        console.log("Year: " + response.data.Year);
-        console.log("Rated: " + response.data.imdbRating);
-        console.log("Country: " + response.data.Country);
-        console.log("Language: " + response.data.Language);
-        console.log("Plot: " + response.data.Plot);
-        console.log("Actors: " + response.data.Actors);
-        console.log("Rotten Tomatoes: " + response.data.Ratings[1].Value);
+        if (second_command === "Mr.Nobody" || second_command === "Mr.+Nobody") {
+            console.log("-----------------------");
+            console.log("If you haven't watched 'Mr. Nobody,' then you should: http://www.imdb.com/title/tt0485947/");
+            console.log("It's on Netflix!");
+        } else {
+            console.log("Title: " + response.data.Title);
+            console.log("Year: " + response.data.Year);
+            console.log("Rated: " + response.data.imdbRating);
+            console.log("Country: " + response.data.Country);
+            console.log("Language: " + response.data.Language);
+            console.log("Plot: " + response.data.Plot);
+            console.log("Actors: " + response.data.Actors);
+            console.log("Rotten Tomatoes: " + response.data.Ratings[1].Value);
+        }
+
     })
+
 }
 
-// function do_what() {
+function do_what() { //works
 
-//     fs.readFile("./random.txt", "utf8", function (err, response) {
-//         if (err) return console.log("An Error Occurred: ", err)
-//         console.log(response)
-//         var txt = response.split(',').pop("")
-//         console.log(txt[1])
+    fs.readFile("./random.txt", "utf8", function (err, response) {
+        if (err) return console.log("An Error Occurred: ", err)
+        var song = response.split(',')
+        console.log(song[1])
 
-//         spotify_this("I Want it That Way")
-//     });
-// }
+        spotify_this(song[1])
+    });
+}
 
 // Choose a user_Command
 switch (user_command) {
@@ -107,7 +108,11 @@ switch (user_command) {
         break;
 
     case "spotify-this-song":
-        spotify_this();
+        if (second_command) {
+            spotify_this(second_command);
+        } else {
+            spotify_this("I want it that way");
+        }
         break;
 
     case "movie-this":
